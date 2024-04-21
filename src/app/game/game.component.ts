f23ef0acd0b4675e12df22a24037064901f01952
Bort190 <heiko.nevoigt@googlemail.com>
2024-04-21 16:12:03 +0200

    cards

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+
+  pickCardAnimation = false;
+  game: Game | undefined;
+
+  ngOnInit():void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+}
